ac422e5b0fddf161fbf07aee1ce9d50c4b04260f
ShivamSharma95 <itsshivam95@gmail.com>
2025-10-11 15:45:00 +0530

    fix

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -4,6 +4,7 @@ public class AddBinary {
         System.out.println(addBinary("11", "1"));
     }
 
+
     public static String addBinary(String a, String b) {
         int ai = a.length() - 1;
         int bi = b.length() - 1;
